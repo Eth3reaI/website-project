ce10b413199894f5173981870adf7b5540012a85
DarkH3X <webh4x@gmail.com>
2021-11-12 21:46:56 +0100

    add website placeholder first release

---
+++ Scripts/links.js
@@ -0,0 +1,3 @@
+let portfolioLink = "https://pol9.notion.site/App-Portfolio-dba596c2bf824ed1ab5b66639d26c0ea"
+let linkedInLink = "https://www.linkedin.com/in/simoneformisano/"
+let gitLabLink = "https://gitlab.com/Eth3real"
